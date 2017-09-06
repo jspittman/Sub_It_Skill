c36226485667bf07156e2c158455e19bc73d8eab
Jeff Pittman <jeff.pittman@gmail.com>
2017-09-05 21:10:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -85,11 +85,9 @@ def help_reponse():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
     
-
+'''
 def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
+
 
     card_title = intent['name']
     session_attributes = {}
@@ -105,7 +103,7 @@ def set_color_in_session(intent, session):
         reprompt_text = "Would you like another sub?"
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
-
+'''
 
 def get_color_from_session(intent, session):
     session_attributes = {}
@@ -397,9 +395,7 @@ def on_intent(intent_request, session):
 
     # Dispatch to your skill's intent handlers
 
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
+    if intent_name == "WhatsMyColorIntent":
         return get_color_from_session(intent, session)
     elif intent_name == "RequestIntent":
         return not_found()
